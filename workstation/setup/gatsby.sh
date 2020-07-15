df3d880ba923e83c03b670158e8fd118d1c3e754
dev <dev@local>
2020-07-15 07:16:58 -0700

    [15/07/2020 07:16:58] Gatsby setup & git pull checksum skip

--- workstation/setup/gatsby.sh
+++ workstation/setup/gatsby.sh
@@ -6,8 +6,9 @@ set -e
 
 ETC_PROFILE="/etc/profile"
 source $ETC_PROFILE &> /dev/null
+source /root/.bashrc &> /dev/null
 
-SETUP_CHECK="$KIRA_SETUP/gatsby-v1" 
+SETUP_CHECK="$KIRA_SETUP/gatsby-v2" 
 if [ ! -f "$SETUP_CHECK" ] ; then
     echo "INFO: Gatsby and its dependencies..."
     echo "INFO: Installing brew..."
@@ -16,7 +17,17 @@ if [ ! -f "$SETUP_CHECK" ] ; then
     curl https://raw.githubusercontent.com/Homebrew/install/master/install.sh -o $BREW_INSTALL
     chmod 777 $BREW_INSTALL
     echo | /bin/su -c "$BREW_INSTALL" - $KIRA_USER
+    echo "INFO: Installing node..."
+    /bin/su -c "brew install node" - $KIRA_USER
+    echo "INFO: Installing nvm..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    source /root/.bashrc
+     echo "INFO: Installing gatsby..."
+    npm install -g gatsby-cli
+    gatsby -v
     touch $SETUP_CHECK
 else
-    echo "INFO: Gatsby was already installed"
+    echo "INFO: NVM v$(nvm --version) was already installed"
+    echo "INFO: node $(node --version) was already installed"
+    echo "INFO: $(gatsby -v) was already installed"
 fi
