95e63edb9cd5f9fdfd9b894b544de4bdf29836c9
dev <dev@local>
2020-08-21 18:03:03 -0700

    [21/08/2020 18:03:03] no upstream host

--- docker/tools-image/scripts/local-cors-proxy-grpc.sh
+++ docker/tools-image/scripts/local-cors-proxy-grpc.sh
@@ -30,7 +30,6 @@ if (\$request_method = 'OPTIONS') {
 grpc_pass $PROXY_PASS;
 
 grpc_set_header X-Real-IP \$remote_addr;
-grpc_set_header Host \$upstream_host;
 grpc_set_header X-Forwarded-For \$upstream_x_forwarded_for;
 grpc_set_header X-Forwarded-Proto \$upstream_x_forwarded_proto;
 grpc_set_header X-Forwarded-Host \$upstream_x_forwarded_host;
